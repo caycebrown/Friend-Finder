e2645ac8032d8d19299d0f1d4fb8bef7cb415800
cayce brown <caycebrown98@gmail.com>
2019-06-02 13:46:45 -0500

    added routes in htmlRoutes and apiRoutes and exported to server.js

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const express = require("express");
 
 const app = express()
 
+const page = require("./app/routing/htmlRoutes.js");
+
+const api = require("./app/routing/apiRoutes.js");
+
+const dataFile = require("./app/data/friends.js");
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,5 +21,15 @@ app.listen(PORT, function(){
 });
 
 app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "app/public/home.html"));
+    res.sendFile(path.join(__dirname, page.home));
 });
+
+app.get("/survey", function(req, res){
+    res.sendFile(path.join(__dirname, page.survey));
+});
+
+app.get("/api/data", function(req, res){
+    res.json(dataFile.userData);
+});
+
+
