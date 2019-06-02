413dccd95933f2f59f1a62a04bceb1dcc8adcd6c
cayce brown <caycebrown98@gmail.com>
2019-06-02 14:51:15 -0500

    updated .post route to allow user form data from survey page to be saved in temp array memory

--- server.js
+++ server.js
@@ -10,16 +10,22 @@ const api = require("./app/routing/apiRoutes.js");
 
 const dataFile = require("./app/data/friends.js");
 
+const PORT = process.env.PORT || 3000;
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function(){
     console.log('App is listening on port: ' + PORT);
 });
 
+
+
+//========================================================================
+//Routing Methods
+//========================================================================
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, page.home));
 });
@@ -33,3 +39,11 @@ app.get("/api/data", function(req, res){
 });
 
 
+//========================================================================
+//Route for posting user submitted data into array declared in friends.js
+//========================================================================
+app.post("/api/data", function(req, res){
+    var userRes = req.body;
+    dataFile.userData.push(userRes);
+    res.json(userRes);
+});
