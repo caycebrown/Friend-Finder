9fcc52d37a926666d35c1da506f19fcb6db494fd
cayce brown <caycebrown98@gmail.com>
2019-06-06 12:03:25 -0500

    populated friends.js with some default json objects to allow match making logic to find a match

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,7 +3,7 @@ var exports = module.exports = {};
 
 exports.userData = {
     users: [
-        {name: 'cayce', img: 'https://404store.com/2017/08/15/4949794-random-image.jpg', scores: [1,1,1,1,1,1,1,1,1,1]}, 
-        {name: 'Herbert', img: 'someimg.jpg', scores: [5,5,5,5,5,5,5,5,5,5]}
+        {name: 'Sassy Sasquatch', img: 'https://is3-ssl.mzstatic.com/image/thumb/Purple71/v4/de/49/e3/de49e314-0f52-fa2c-7e6e-11b7dcbd76b2/source/512x512bb.jpg', scores: [1,1,1,1,1,1,1,1,1,1]}, 
+        {name: 'Herbert', img: 'https://i1.sndcdn.com/artworks-000089420936-77d6ti-t500x500.jpg', scores: [5,5,5,5,5,5,5,5,5,5]}
     ]
 };
