93677eb6b5f2e7c1dfb931214716452ae4b0377d
cayce brown <caycebrown98@gmail.com>
2019-06-06 11:43:43 -0500

    updated modal to include the match results name and image

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,7 +3,7 @@ var exports = module.exports = {};
 
 exports.userData = {
     users: [
-        {name: 'cayce', img: 'test.jpg', scores: [1,1,1,1,1,1,1,1,1,1]}, 
+        {name: 'cayce', img: 'https://404store.com/2017/08/15/4949794-random-image.jpg', scores: [1,1,1,1,1,1,1,1,1,1]}, 
         {name: 'Herbert', img: 'someimg.jpg', scores: [5,5,5,5,5,5,5,5,5,5]}
     ]
 };
